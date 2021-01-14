06502ef09c02de55c77b41303eb14ffdd3a4af67
mark.zhang <38938010+mark-zh@users.noreply.github.com>
2021-01-14 11:01:03 +0800

    Create crt.sh

---
+++ crt.sh
@@ -0,0 +1,1 @@
+curl -fsSL -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:69.0) Gecko/20100101 Firefox/69.0" "https://crt.sh/?CN=%25.$1" | sort -n | uniq -c | grep -o -P '(?<=\<TD\>).*(?=\<\/TD\>)' | sed -e '/white-space:normal/d'
